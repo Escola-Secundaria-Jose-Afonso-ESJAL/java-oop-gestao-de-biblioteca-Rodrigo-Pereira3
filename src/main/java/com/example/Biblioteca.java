3fb7843a8548e75184b2071315d71c4ca5d0a5c6
RODRIGOFILIPESANTOSP <37608@esjaloures.org>
2026-02-11 12:01:57 +0000

    S1.

--- src/main/java/com/example/Biblioteca.java
+++ src/main/java/com/example/Biblioteca.java
@@ -1,24 +1,45 @@
-package  com.example;
-
+package  com.example
 import java.util.ArrayList;
 
 public class Biblioteca {
     private static final int MAX_LIVROS = 100; // Variável de classe
-    private ArrayList<Livro> livros; // Variável de instância
+    private ArrayList<Livro> livros;// Variável de instância
 
     public Biblioteca() {
         this.livros = new ArrayList<>();
     }
 
     public boolean adicionarLivro(Livro livro) {
-        
+        for(Livro ltemp : livros)
+        {
+            if(ltemp.getTitulo() == livro.getTitulo())
+            {
+                return false;
+            }
+        }
+        livros.add(livro);
+        return true;
     }
 
     public boolean removerLivro(String titulo) {
-        
+        for(Livro ltemp : livros)
+        {
+            if(ltemp.getTitulo() == titulo)
+            {
+                livros.remove(ltemp);
+                return true;
+            }
+        }
+        return false;
     }
 
     public void listarLivros() {
-        
+        System.out.println("Livros disponíveis:");
+        for (Livro livro : livros) 
+        {
+            System.out.println(
+            " - " + livro.getTitulo() +  " / " + livro.getAutor() +" / " + livro.getAno()
+            );
+        }
     }
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,21 @@
-package com.example;
-
+package com.example
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
+        Biblioteca bibliotecaDaEsquina = new Biblioteca();
         
+            ArrayList<Livro> livros = new ArrayList<Livro>();
+            Livro L1 = new Livro("O Senhor dos Anéis","J.R.R. Tolkien",1954);
+            Livro L2 = new Livro("Duna","Frank Herbert",1965);
+            Livro L3 = new Livro("1984","George Orwell",1949);
+
+            livros.add(L1);
+            livros.add(L2);
+            livros.add(L3);
+            
+            System.out.println(livros);
+            
+        
+        bibliotecaDaEsquina.listarLivros();
     }
 }
